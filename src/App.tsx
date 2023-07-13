bbf09f74f2d5ea41968a602ba3dea1bb637d338e
Davi <davi.ribeirodosanjos@gmail.com>
2023-07-13 11:24:33 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { CustomProvider } from 'rsuite';
+import ptBr from 'rsuite/locales/pt_BR';
+
+import { MainRoutes } from "./routes"
+import { MainHeader } from './components/header';
+
+
 export function App() {
+  console.log('app')
 
   return (
-    <h1>
-      Test
-    </h1>
+    <CustomProvider theme="dark" locale={ptBr}>
+      <MainHeader />
+      <MainRoutes />
+    </CustomProvider>
   )
 }
