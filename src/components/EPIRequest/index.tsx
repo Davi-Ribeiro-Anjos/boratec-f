4bc578374f37e4350d180a1fb16d64324b1e52c4
Davi <davi.ribeirodosanjos@gmail.com>
2023-08-30 17:36:14 -0300

    adicionando enviar

--- src/components/EPIRequest/index.tsx
+++ src/components/EPIRequest/index.tsx
@@ -1,7 +1,17 @@
+import { EPIRequestConfirm } from "./EPIRequestConfirm";
+import { EPIRequestCreate } from "./EPIRequestCreate";
 import { EPIRequestFilter } from "./EPIRequestFilter";
+import { EPIRequestHeader } from "./EPIRequestHeader";
+import { EPIRequestSend } from "./EPIRequestSend";
+import { EPIRequestView } from "./EPIRequestView";
 
 
 
 export const EPIRequest = {
-    Filter: EPIRequestFilter
+    Header: EPIRequestHeader,
+    Filter: EPIRequestFilter,
+    Create: EPIRequestCreate,
+    View: EPIRequestView,
+    Send: EPIRequestSend,
+    Confirm: EPIRequestConfirm
 }
