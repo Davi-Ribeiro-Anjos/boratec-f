adc8d385453be1bf3e2062ba247866393f280f95
Davi <davi.ribeirodosanjos@gmail.com>
2023-09-26 10:29:10 -0300

    removendo sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "bora-transportes-50",
-    project: "frontend"
-  })],
-
-  build: {
-    sourcemap: true
-  }
+  plugins: [react()],
 })
